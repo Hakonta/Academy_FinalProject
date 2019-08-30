4a758201b310fcbe31221a6f228119fe1562bbb5
aniajohansen <aniajohansen@gmail.com>
2019-08-30 13:02:28 +0200

    Added map  successfully

--- Academy_FinalProject/ClientApp/src/App.js
+++ Academy_FinalProject/ClientApp/src/App.js
@@ -1,20 +1,33 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router';
-import { Layout } from './components/Layout';
-import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
-
-export default class App extends Component {
-  static displayName = App.name;
-
-  render () {
+import GoogleMapReact from 'google-map-react';
+ 
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+ 
+class SimpleMap extends Component {
+  static defaultProps = {
+    center: {
+      lat: 59.946548,
+      lng: 10.766970
+    },
+    zoom: 18
+  };
+ 
+  render() {
     return (
-      <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data' component={FetchData} />
-      </Layout>
+      // TODO: Få på auto-tracking på vår plassering
+      // Important! Always set the container height explicitly
+      <div style={{ height: '100vh', width: '100%' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: 'AIzaSyAooUPBzTGsLMvMXUvUsLxSBR0D6gTAQk8' }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+        >
+          <AnyReactComponent text='some text' lat='59.946548' lng='10.766970'/>
+        </GoogleMapReact>
+      </div>
     );
   }
 }
+// const Marker = props => {return <div className="SUPERAWSM"></div>}
+ 
+export default SimpleMap;
--- Academy_FinalProject/ClientApp/src/index.js
+++ Academy_FinalProject/ClientApp/src/index.js
@@ -5,13 +5,5 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
-const rootElement = document.getElementById('root');
-
-ReactDOM.render(
-  <BrowserRouter basename={baseUrl}>
-    <App />
-  </BrowserRouter>,
-  rootElement);
-
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
