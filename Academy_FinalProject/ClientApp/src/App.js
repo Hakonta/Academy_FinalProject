3096fae2d72993399127a8ed1f2cd136501129d4
aniajohansen <aniajohansen@gmail.com>
2019-08-30 15:09:03 +0200

    made a kick-ass user location

--- Academy_FinalProject/ClientApp/src/App.js
+++ Academy_FinalProject/ClientApp/src/App.js
@@ -1,33 +1,64 @@
 import React, { Component } from 'react';
-import GoogleMapReact from 'google-map-react';
- 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
- 
-class SimpleMap extends Component {
-  static defaultProps = {
-    center: {
-      lat: 59.946548,
-      lng: 10.766970
-    },
-    zoom: 18
+import { Map, GoogleApiWrapper,InfoWindow, Marker } from 'google-maps-react';
+import CurrentLocation from './Map';
+import ReactDOM from 'react-dom';
+
+const mapStyles = {
+  map:{
+    position:'absolute',
+    width: '100%',
+    height: '100%'
+  }
+};
+
+export class MapContainer extends Component {
+    state = {
+     showingInfoWindow: false,  //Hides or the shows the infoWindow
+     activeMarker: {},          //Shows the active marker upon click
+     selectedPlace: {}          //Shows the infoWindow to the selected place upon a marker
+   };
+
+   onMarkerClick = (props, marker, e) =>
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+
+  onClose = props => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      });
+    }
   };
- 
+  
   render() {
     return (
-      // TODO: Få på auto-tracking på vår plassering
-      // Important! Always set the container height explicitly
-      <div style={{ height: '100vh', width: '100%' }}>
-        <GoogleMapReact
-          bootstrapURLKeys={{ key: 'AIzaSyAooUPBzTGsLMvMXUvUsLxSBR0D6gTAQk8' }}
-          defaultCenter={this.props.center}
-          defaultZoom={this.props.zoom}
+      
+      <CurrentLocation
+      centerAroundCurrentLocation
+      google={this.props.google} >
+        <Marker
+          onClick={this.onMarkerClick}
+          name={'You are here'}
+        />
+        <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}
+          onClose={this.onClose}
         >
-          <AnyReactComponent text='some text' lat='59.946548' lng='10.766970'/>
-        </GoogleMapReact>
-      </div>
+          <div>
+            <h4>{this.state.selectedPlace.name}</h4>
+          </div>
+        </InfoWindow>
+
+      </CurrentLocation>
     );
   }
 }
-// const Marker = props => {return <div className="SUPERAWSM"></div>}
- 
-export default SimpleMap;
+
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyAooUPBzTGsLMvMXUvUsLxSBR0D6gTAQk8'
+})(MapContainer);
