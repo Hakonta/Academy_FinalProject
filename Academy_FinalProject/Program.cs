e2fb8c9e03de9a3c2aa9a14428e3849fa06e4d67
hakonta <haakon_tangen@hotmail.com>
2019-08-30 12:06:22 +0200

    Added a class and a general method to fetch scooterdata. Tested on Tier's API and received confirmation that our attempt to fetch scooterdata from Oslo was succesful.

--- Academy_FinalProject/Program.cs
+++ Academy_FinalProject/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Academy_FinalProject.ImportAPI;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -14,6 +15,8 @@ namespace Academy_FinalProject
     {
         public static void Main(string[] args)
         {
+            FetchData fetchData = new FetchData();
+            fetchData.FetchScooterData();
             CreateWebHostBuilder(args).Build().Run();
         }
 
