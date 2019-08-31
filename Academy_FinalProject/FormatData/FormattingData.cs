efb79ccc5bbd813acd6a22eb2342f6d4e190cb47
troy91 <troy.tran@hotmail.com>
2019-08-31 10:39:56 +0200

    Added FormatData folder with FormattingData.cs

---
+++ Academy_FinalProject/FormatData/FormattingData.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Academy_FinalProject.FormatData;
+using Academy_FinalProject.ImportAPI;
+using Newtonsoft.Json.Linq;
+
+namespace Academy_FinalProject.FormatData
+{
+    public class FormattingData
+
+    {
+        string tierKeyType = "X-Api-Key";
+        string tierKeyName = "bpEUTJEBTf74oGRWxaIcW7aeZMzDDODe1yBoSxi2";
+        string tierUrl = $"https://platform.tier-services.io/vehicle?zoneId=OSLO";
+
+        FetchTierData fetchTierData = new FetchTierData();
+
+        private Task<JObject> jsonObj;
+
+        public FormattingData()
+        {
+            jsonObj = fetchTierData.FetchScooterData(tierUrl, tierKeyType, tierKeyName);
+        }
+
+
+
+
+
+
+
+
+    }
+}
