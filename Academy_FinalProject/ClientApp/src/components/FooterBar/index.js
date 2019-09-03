2fb6be861f7ef82da2e214ff493bac98df85ca8c
aniajohansen <aniajohansen@gmail.com>
2019-09-03 15:03:39 +0200

    some minor changes

--- Academy_FinalProject/ClientApp/src/components/FooterBar/index.js
+++ Academy_FinalProject/ClientApp/src/components/FooterBar/index.js
@@ -1,40 +1,38 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
 import {Navbar} from 'react-bootstrap';
 
-const styles={
-  color:'grey',
-}
+
 const styles2={
   color:'grey',
   textAlign: 'center',
   fontSize: '10px',
   display: 'center',
-  height: '70px'
+  height: '70px',
+  display: 'flex',
+  justifyContent: 'center'
   
 }
 
-const center={display: 'flex', justifyContent: 'center'}
 
 export class FooterBar extends React.Component{
   render(){
   return (
       <div className="fixed-bottom">
       <Navbar bg="dark" expand="lg" fixed='bottom'>
-          {/* <Nav.Link href="bruker" style={styles}>Bruker</Nav.Link> */}
-        <div style ={center}>
-          <ButtonToolbar style={styles}>
-            <Button variant="outline-secondary" size="sm">QR Scan</Button>
-            <Button variant="outline-secondary" size="sm">Nearby</Button>
+
+          <ButtonToolbar style={styles2}>
+            <Button variant="outline-secondary" size="lg">QR Scan</Button>
+            <Button variant="outline-secondary" size="lg">Nearby</Button>
           </ButtonToolbar>
-        </div>
-    
-        <Card.Footer className="text-muted" style={styles2}>This webpage was created by Academy students from batch #1</Card.Footer>
+
+        {/* <div className="fixed-bottom"> */}
+        {/* <Card.Footer className="text-muted" style={{fontSize: '10px'}}>This webpage was created by Academy students from batch #1</Card.Footer> */}
+       {/* </div> */}
         </Navbar>
-    </div>
+      </div>
 );
 }}
 
