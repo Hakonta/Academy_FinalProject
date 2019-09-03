82e0a66842d3a5902e8c09a33ec4ae6986aa24d6
aniajohansen <aniajohansen@gmail.com>
2019-09-03 16:59:20 +0200

    minor changes to footer

--- Academy_FinalProject/ClientApp/src/components/FooterBar/index.js
+++ Academy_FinalProject/ClientApp/src/components/FooterBar/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
-import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
-import { Navbar } from 'react-bootstrap';
+import { Navbar, Card } from 'react-bootstrap';
 
 const styles2={
   color:'grey',
@@ -27,9 +26,9 @@ export class FooterBar extends React.Component{
             <Button variant="outline-secondary" size="lg">Nearby</Button>
          
 
-        {/* <div className="fixed-bottom"> */}
-        {/* <Card.Footer className="text-muted" style={{fontSize: '10px'}}>This webpage was created by Academy students from batch #1</Card.Footer> */}
-       {/* </div> */}
+        {/* <div className="fixed-bottom">
+        <Card.Footer className="text-muted" style={{fontSize: '10px'}}>This webpage was created by Academy students from batch #1</Card.Footer>
+       </div> */}
        <br></br>
         </Navbar>
       </div>
