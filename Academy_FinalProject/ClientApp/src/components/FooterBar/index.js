6101c89e34532e3ed3a8066c9d1e3614a0681884
aniajohansen <aniajohansen@gmail.com>
2019-09-03 15:33:52 +0200

    centered buttons a little

--- Academy_FinalProject/ClientApp/src/components/FooterBar/index.js
+++ Academy_FinalProject/ClientApp/src/components/FooterBar/index.js
@@ -18,16 +18,19 @@ export class FooterBar extends React.Component{
   render(){
   return (
       <div className="fixed-bottom">
-      <Navbar bg="dark" expand="lg" fixed='bottom'>
+      <Navbar bg="dark" expand="xl" fixed='bottom'>
+
+        <br></br>
         {/* TODO: her burde vi få sentrert de to knappene(QR scan og nearby) */}
-          <ButtonToolbar style={styles2}>
+    
             <Button variant="outline-secondary" size="lg">QR Scan</Button>
             <Button variant="outline-secondary" size="lg">Nearby</Button>
-          </ButtonToolbar>
+         
 
         {/* <div className="fixed-bottom"> */}
         {/* <Card.Footer className="text-muted" style={{fontSize: '10px'}}>This webpage was created by Academy students from batch #1</Card.Footer> */}
        {/* </div> */}
+       <br></br>
         </Navbar>
       </div>
 );
