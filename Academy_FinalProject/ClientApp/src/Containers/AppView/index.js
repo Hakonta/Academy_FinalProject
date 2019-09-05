c3535654c172d30c7c1575d0abb8a2aac7ddc660
hakonta <haakon_tangen@hotmail.com>
2019-09-05 16:22:17 +0200

    .

--- Academy_FinalProject/ClientApp/src/Containers/AppView/index.js
+++ Academy_FinalProject/ClientApp/src/Containers/AppView/index.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { HeaderBar } from '../../Components/HeaderBar';
-<<<<<<< HEAD
 import {FooterBar} from '../../Components/FooterBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import voiDot from '../../Assets/voi-dot.png'
@@ -43,10 +42,6 @@ export class MapContainer extends Component {
       })
       .catch((error) => { console.log(error); });
   }
-=======
-import { FooterBar } from '../../Components/FooterBar';
-import MapBase from '../../Components/MapBaseView'
->>>>>>> 7f95eb21d1cb01941971662d32473073a31e8f1d
 
 
 export class AppView extends Component {
@@ -54,7 +49,6 @@ export class AppView extends Component {
   render() {
     return (
       <div>
-<<<<<<< HEAD
       <HeaderBar/>
       <CurrentLocation
       centerAroundCurrentLocation
@@ -123,13 +117,6 @@ export class AppView extends Component {
     <FooterBar/>
     </div>
        
-=======
-        <HeaderBar />
-          <MapBase />
-          {/* SIDEBAR COMPONENT HERE */}
-        <FooterBar />
-      </div>
->>>>>>> 7f95eb21d1cb01941971662d32473073a31e8f1d
     );
   }
 }
