7c42b165991d4235361ee90dc7b3df9f82729253
Cipo87 <jacob_aspelin@hotmail.com>
2019-09-05 09:07:23 +0200

    Removed all warnings in backend with help from Anders

--- Academy_FinalProject/Controllers/ScooterController.cs
+++ Academy_FinalProject/Controllers/ScooterController.cs
@@ -4,7 +4,6 @@ using System.Diagnostics;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
-using System.Web.Http.Cors;
 using System.Xml;
 using Academy_FinalProject.FormatData;
 using Academy_FinalProject.ImportAPI;
