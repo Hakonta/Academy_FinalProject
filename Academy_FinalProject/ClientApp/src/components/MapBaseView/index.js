ffe36612fd1a50735cc8e6a1bba439c8618a0405
hakonta <haakon_tangen@hotmail.com>
2019-09-05 21:08:15 +0200

    .

--- Academy_FinalProject/ClientApp/src/components/MapBaseView/index.js
+++ Academy_FinalProject/ClientApp/src/components/MapBaseView/index.js
@@ -118,7 +118,7 @@ export default class MapBaseLayer extends Component {
             {
               (clusterer) => this.state.scooters.map((scooter, index) => (
                 <div key={index}>
-                  <ScooterMarker provider={scooter.providerName} position={{ lat: scooter.latitude, lng: scooter.longitude }} clusterer={clusterer} markerClicked={() => { this.setState({ selectedScooter: scooter }) }} />
+                  <ScooterMarker provider={scooter.providerName} position={{ lat: scooter.latitude, lng: scooter.longitude }} clusterer={clusterer} markerClicked={() => { this.setState({ selectedScooter: scooter }) }}/>
                 </div>
               ))
             }
