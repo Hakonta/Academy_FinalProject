8f82377928476b10b61127754e20003655d86a80
aniajohansen <aniajohansen@gmail.com>
2019-09-05 14:54:15 +0200

    Added Zvipp to our Scooters

--- Academy_FinalProject/FormatData/FormattingData.cs
+++ Academy_FinalProject/FormatData/FormattingData.cs
@@ -1,71 +1,71 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Academy_FinalProject.FormatData;
-using Academy_FinalProject.ImportAPI;
-using Academy_FinalProject.Models;
-using Newtonsoft.Json.Linq;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+//using Academy_FinalProject.FormatData;
+//using Academy_FinalProject.ImportAPI;
+//using Academy_FinalProject.Models;
+//using Newtonsoft.Json.Linq;
 
-namespace Academy_FinalProject.FormatData {
-    public class FormattingData {
+//namespace Academy_FinalProject.FormatData {
+//    public class FormattingData {
         
 
         
-        public List<Scooter> ExtractScooterInfoToList(JObject rawScooterJsonData) {
-            //Method takes a JObject (FetchScooterData() ) and return a C# list of scooters.
-            List<Scooter> allScooters = new List<Scooter>();
+//        public List<Scooter> ExtractScooterInfoToList(JObject rawScooterJsonData) {
+//            //Method takes a JObject (FetchScooterData() ) and return a C# list of scooters.
+//            List<Scooter> allScooters = new List<Scooter>();
 
-            JToken data = rawScooterJsonData["data"];
-            foreach (var scooter in data) {
+//            JToken data = rawScooterJsonData["data"];
+//            foreach (var scooter in data) {
 
-                allScooters.Add(new Scooter {
-                    ProviderName = "Tier",
-                    Latitude = (double)scooter["lat"],
-                    Longitude = (double)scooter["lng"],
-                    BatteryCapacity = (int)scooter["batteryLevel"],
-                    IsAvailable = (bool)scooter["isRentable"]
-                });
-            }
-            return allScooters;
-        }
+//                allScooters.Add(new Scooter {
+//                    ProviderName = "Tier",
+//                    Latitude = (double)scooter["lat"],
+//                    Longitude = (double)scooter["lng"],
+//                    BatteryCapacity = (int)scooter["batteryLevel"],
+//                    IsAvailable = (bool)scooter["isRentable"]
+//                });
+//            }
+//            return allScooters;
+//        }
 
-        public List<Scooter> ExtractScooterInfoToList(JArray rawScooterJsonData, List<Scooter> allScooters)
-        {
-            //Method takes a JObject (FetchScooterData() ) and return a C# list of scooters.
+//        public List<Scooter> ExtractScooterInfoToList(JArray rawScooterJsonData, List<Scooter> allScooters)
+//        {
+//            //Method takes a JObject (FetchScooterData() ) and return a C# list of scooters.
 
-            //JToken data = rawScooterJsonData["data"];
-            foreach (var scooter in rawScooterJsonData)
-            {
+//            //JToken data = rawScooterJsonData["data"];
+//            foreach (var scooter in rawScooterJsonData)
+//            {
 
-                allScooters.Add(new Scooter
-                {
-                    ProviderName = "Voi",
-                    BatteryCapacity = (int)scooter["battery"],
-                    Latitude = (double)scooter["location"].First,
-                    Longitude = (double)scooter["location"][1]
-                });
-            }
-            return allScooters;
+//                allScooters.Add(new Scooter
+//                {
+//                    ProviderName = "Voi",
+//                    BatteryCapacity = (int)scooter["battery"],
+//                    Latitude = (double)scooter["location"].First,
+//                    Longitude = (double)scooter["location"][1]
+//                });
+//            }
+//            return allScooters;
 
-        }
-        public List<Bysykkel> ExtractBysyklerInfoToList(BysykkelResponse rawBikeJsonData)
-        {
-            List<Bysykkel> allBikes = new List<Bysykkel>();
+//        }
+//        public List<Bysykkel> ExtractBysyklerInfoToList(BysykkelResponse rawBikeJsonData)
+//        {
+//            List<Bysykkel> allBikes = new List<Bysykkel>();
 
-            foreach (var bike in rawBikeJsonData.Data.Stations)
-            {
+//            foreach (var bike in rawBikeJsonData.Data.Stations)
+//            {
 
-                allBikes.Add(new Bysykkel
-                {
-                    StationName = bike.Name,
-                    Latitude = bike.Lat,
-                    Longitude = bike.Lon,
-                    StationCapacity = bike.Capacity,
-                    // TODO: Show bikes available?
-                });
-            }
-            return allBikes;
-        }
-    }
-}
+//                allBikes.Add(new Bysykkel
+//                {
+//                    StationName = bike.Name,
+//                    Latitude = bike.Lat,
+//                    Longitude = bike.Lon,
+//                    StationCapacity = bike.Capacity,
+//                    // TODO: Show bikes available?
+//                });
+//            }
+//            return allBikes;
+//        }
+//    }
+//}
