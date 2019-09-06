80270663b2e4a4040985e1a865c2644951b2f442
Cipo87 <jacob_aspelin@hotmail.com>
2019-09-06 10:38:21 +0200

    Started with creating a sortCard

---
+++ Academy_FinalProject/ClientApp/src/components/SortCard/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const sortCard = () => {
+    return(
+        <div style={styles.card}>
+
+        </div>
+
+    )
+}
+
+export default sortCard;
+
+const styles = {
+
+    card:{
+        height: 100,
+        width: 100,
+        color: 'white'
+    }
+}
