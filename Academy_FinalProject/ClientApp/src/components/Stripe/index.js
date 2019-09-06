afb5be6d89273473feecba67f7bf640733a14140
hakonta <aniajohansen@gmail.com>
2019-09-06 10:24:52 +0200

    Added a stripe-folder

---
+++ Academy_FinalProject/ClientApp/src/components/Stripe/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import StripeCheckout from 'react-stripe-checkout';
+
+export default class Checkout extends React.Component {
+  onToken = (token, addresses) => {
+    // TODO: Send the token information and any other
+    // relevant information to your payment process
+    // server, wait for the response, and update the UI
+    // accordingly. How this is done is up to you. Using
+    // XHR, fetch, or a GraphQL mutation is typical.
+  };
+
+  render() {
+    return (
+      <StripeCheckout
+        stripeKey="your_PUBLISHABLE_stripe_key"
+        token={this.onToken}
+      />
+    )
+  }
+}
