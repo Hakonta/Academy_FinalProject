e7cc197fc0b6114176e8ebbd94f10ee17892b331
hakonta <haakon_tangen@hotmail.com>
2019-09-06 14:55:12 +0200

    Made minor changes to the RideCard

--- Academy_FinalProject/ClientApp/src/components/RideCard/index.js
+++ Academy_FinalProject/ClientApp/src/components/RideCard/index.js
@@ -1,9 +1,22 @@
 import React from 'react'
 import CounterComponent from '../CounterComponent'
 
+const styles = {
+    outerContainer: 
+    {
+        display:"flex"
+
+    },
+    container: 
+    {
+        display:"flex", flexDirection:"column", alignItems:"center"
+    }
+}
+
 export default function showRideDetails() 
 {
 return (
+
     <div id='container'><h1>Have a safe ride!</h1><br></br>
     <h2>Time passed</h2><br></br>
     <h2><CounterComponent/></h2>
