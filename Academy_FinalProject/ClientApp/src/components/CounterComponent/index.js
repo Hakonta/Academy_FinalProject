695d07640859eaa1b2b3e16639cc690d4d2d7e5f
hakonta <haakon_tangen@hotmail.com>
2019-09-07 16:17:20 +0200

    WIP calculating the prize after each fare

--- Academy_FinalProject/ClientApp/src/components/CounterComponent/index.js
+++ Academy_FinalProject/ClientApp/src/components/CounterComponent/index.js
@@ -1,16 +1,30 @@
 import React from 'react'
 
+const styles = {
+    startButton: {
+        backgroundColor:"darkGreen", color:"white"
+    },
+    stopButton: {
+        backgroundColor:"darkRed", color:"white"
+    }
+}
+
 class CounterComponent extends React.Component {
   constructor(props){
     super(props)
     this.state = {
       time: 0,
+      start: 0,
+      hours: 0,
+      minutes: 0,
+      seconds: 0,
+      price: 0,
       isOn: false,
-      start: 0
+
     }
     this.startTimer = this.startTimer.bind(this)
     this.stopTimer = this.stopTimer.bind(this)
-    this.resetTimer = this.resetTimer.bind(this)
+    // this.resetTimer = this.resetTimer.bind(this)
   }
   startTimer() {
     this.setState({
@@ -25,30 +39,60 @@ class CounterComponent extends React.Component {
   stopTimer() {
     this.setState({isOn: false})
     clearInterval(this.timer)
+    this.calculateSeconds()
+
   }
-  resetTimer() {
-    this.setState({time: 0, isOn: false})
+//   resetTimer() {
+//     this.setState({time: 0, isOn: false})
+//   }
+  calculateSeconds() {
+      let checkSeconds = (this.state.time/1000).toFixed(0);
+      console.log(checkSeconds)
+      
+      if (checkSeconds >= 60 )
+      {
+        this.setState({seconds: 0})
+      }
+      else
+      {
+        this.setState({
+            seconds:(this.state.time/1000).toFixed(0),
+            minutes:((this.state.time/1000)/60).toFixed(0)
+        })
+        console.log(this.state.isOn)
+        console.log(this.state.seconds)
+        console.log(this.state.minutes)
+
+      }
+      
+      console.log(this.state.time);
   }
+  calculatePrice() {
+    console.log('This method runs')  
+    this.setState({price: 0})
+
+  }
+
   render() {
-    let start = (this.state.time == 0) ?
-      <button onClick={this.startTimer}>Unlock Scooter</button> :
+    let start = (this.state.time === 0) ?
+      <button style={styles.startButton} onClick={this.startTimer}>Unlock Scooter</button> :
       null
-    let stop = (this.state.time == 0 || !this.state.isOn) ?
+    let stop = (this.state.time === 0 || !this.state.isOn) ?
       null :
-      <button onClick={this.stopTimer}>End Ride</button>
-    let resume = (this.state.time == 0 || this.state.isOn) ?
-      null :
-      <button onClick={this.startTimer}>resume</button>
-    let reset = (this.state.time == 0 || this.state.isOn) ?
-      null :
-      <button onClick={this.resetTimer}>reset</button>
+      <button style={styles.stopButton} onClick={this.stopTimer}>End Ride</button>
+    // let resume = (this.state.time == 0 || this.state.isOn) ?
+    //   null :
+    //   <button onClick={this.startTimer}>resume</button>
+    // let reset = (this.state.time == 0 || this.state.isOn) ?
+    //   null :
+    //   <button onClick={this.resetTimer}>reset</button>
     return(
       <div>
         <h3>{((this.state.time)/1000).toFixed(0)}s / {(((this.state.time)/1000)/60).toFixed(0)}m / {((((this.state.time)/1000)/60)/60).toFixed(0)}h</h3>
         {start}
-        {resume}
+        {/* {resume} */}
         {stop}
-        {reset}
+        {/* {reset} */}
       </div>
     )
   }
--- Academy_FinalProject/ClientApp/src/components/RideCard/index.js
+++ Academy_FinalProject/ClientApp/src/components/RideCard/index.js
@@ -4,22 +4,23 @@ import CounterComponent from '../CounterComponent'
 const styles = {
     outerContainer: 
     {
-        display:"flex"
+         textAlign:"center"
 
     },
     container: 
     {
-        display:"flex", flexDirection:"column", alignItems:"center"
+        display:"flex", flexDirection:"column", alignItems:"center", justifyContent:"center"
     }
 }
 
 export default function showRideDetails() 
 {
 return (
-
-    <div id='container'><h1>Have a safe ride!</h1><br></br>
-    <h2>Time passed</h2><br></br>
-    <h2><CounterComponent/></h2>
-    </div>
+    <div style={styles.outerContainer}>
+        <div style={styles.container}>
+            <div id='container'><h1>Have a safe ride!</h1>
+                <h2>Time passed</h2>
+                <h2><CounterComponent/></h2>
+    </div></div></div> 
 )
 }
