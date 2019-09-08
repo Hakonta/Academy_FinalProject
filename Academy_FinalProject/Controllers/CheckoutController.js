e8d7d83154d8b1e715eb11895b64df3554c8e1ec
rebekkavaa <rebekka.v.aa@gmail.com>
2019-09-08 13:42:55 +0200

    trying to add the FUCKING STRIPE BACKEND

---
+++ Academy_FinalProject/Controllers/CheckoutController.js
@@ -0,0 +1,52 @@
+const cors = require("cors");
+const express = require("express");
+const stripe = require("stripe")("sk_test_A7K49OYyqLKO37CGfq4oth9y00COum2D6U");
+const uuid = require("uuid/v4");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send();
+});
+
+app.post("/checkout", async (req, res) => {
+  console.log("Request:", req.body);
+
+  let error;
+  let status;
+  try {
+    const { product, token } = req.body;
+
+    const customer = await stripe.customers.create({
+      email: token.email,
+      source: token.id
+    });
+
+    const idempotency_key = uuid();
+    const charge = await stripe.charges.create(
+      {
+        amount: product.price * 100,
+        currency: "usd",
+        customer: customer.id,
+        receipt_email: token.email,
+        description: `Purchased the ${product.name}`,
+        }
+      },
+      {
+        idempotency_key
+      }
+    );
+    console.log("Charge:", { charge });
+    status = "success";
+  } catch (error) {
+    console.error("Error:", error);
+    status = "failure";
+  }
+
+  res.json({ error, status });
+});
+
+app.listen(8080);
