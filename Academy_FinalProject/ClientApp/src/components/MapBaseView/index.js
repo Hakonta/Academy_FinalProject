d0c5c4a741afa59858ca76e1d780dc80419ee8f2
Cipo87 <jacob_aspelin@hotmail.com>
2019-09-08 12:44:10 +0200

    created loadingspinner

--- Academy_FinalProject/ClientApp/src/components/MapBaseView/index.js
+++ Academy_FinalProject/ClientApp/src/components/MapBaseView/index.js
@@ -14,6 +14,7 @@ import { ThemeProvider } from 'react-bootstrap';
 //import CurrentPositionMarker from '../../Components/CurrentPositionMarker'
 import RideCard from '../RideCard';
 import FilterButton from '../FilterButton'
+import LoadingSpinner from '../loadingspinner'
 
 
 
@@ -38,7 +39,8 @@ export default class MapBaseLayer extends Component {
       currentCenter: {
         lat: 60.92,
         lng: 10.723
-      }
+      },
+      mapIsLoadiong: true,
     }
   }
 
@@ -121,9 +123,13 @@ export default class MapBaseLayer extends Component {
         <LoadScript
           id="script-loader"
           googleMapsApiKey="AIzaSyAp2jh1zbAqgoQH8qpd8Af622VYmIdfeVY"
+          onLoad={()=>{console.log('loading..' + this.state.mapIsLoadiong)}}
         >
           <GoogleMap
             id='example-map'
+            onTilesLoaded={()=>{this.setState({mapIsLoadiong: false}) ;console.log('map has loaded.' + this.state.mapIsLoadiong)}}
+            
+            
             options={{
               styles: mapStyle,
               //Toggle buttons on map
@@ -144,6 +150,7 @@ export default class MapBaseLayer extends Component {
               padding: 0,
             }}
           >
+            {this.state.mapIsLoadiong ? <LoadingSpinner /> : null }
             <MarkerClusterer
               averageCenter
               maxZoom={15}
