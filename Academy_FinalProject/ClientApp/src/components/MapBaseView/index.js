c16ff21942cc795a81cc75a67f2fb2faa7a56f4c
hakonta <haakon_tangen@hotmail.com>
2019-09-09 22:07:53 +0200

    Made all the menu options from Ride Cards available in the footer menu.

--- Academy_FinalProject/ClientApp/src/components/MapBaseView/index.js
+++ Academy_FinalProject/ClientApp/src/components/MapBaseView/index.js
@@ -232,10 +232,10 @@ export default class MapBaseLayer extends Component {
               </div>
               </InfoWindow>
             )}
-            
-          {this.state.showScooterFooter ? <InfoCard providerName={this.state.selectedScooter.providerName} battery={this.state.selectedScooter.batteryCapacity} 
+            {this.state.showDefaultCard ?
+          this.state.showScooterFooter ? <InfoCard providerName={this.state.selectedScooter.providerName} battery={this.state.selectedScooter.batteryCapacity} 
           toggleRideCard={() => { this.setState({ showDefaultCard: !this.state.showDefaultCard }) }} /> 
-                  : null} {this.state.showDefaultCard && this.state.showScooterFooter ? null : <RideCard /> }
+                  : null : <RideCard/> }
 
           </GoogleMap>
         </LoadScript>
