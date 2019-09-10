c1c58cb88406d01efd0eec8cf78b52ef4a12d459
rebekkavaa <rebekka.v.aa@gmail.com>
2019-09-10 16:24:46 +0200

    added about page

--- Academy_FinalProject/ClientApp/src/Containers/AboutView/index.js
+++ Academy_FinalProject/ClientApp/src/Containers/AboutView/index.js
@@ -1,23 +1,67 @@
-import React, { Component } from 'react';
 
+import { HeaderBar } from '../../Components/HeaderBar';
+import React, { Component } from 'react';
+import Plx from 'react-plx';
+import about from '../../Assets/about.jpg'
+
+const parallaxData = [
+    {
+      start: 0,
+      end: 550,
+      properties: [
+        {
+          startValue: 1,
+          endValue: 1.4,
+          property: 'scale',
+        },
+      ],
+    },
+  ];
 
 export default class AboutView extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
     }
-  }
+      
+    render() {
+        return (
+            <div>
+                <HeaderBar/>
+                <Plx
+                    className='MyAwesomeParallax'
+                    parallaxData={ parallaxData } // your parallax effects, see documentation
+                >
+                <img src={about} width="100%"/>
+                </Plx>
 
+                <section id="about">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-8 mx-auto">
+                                <br></br>
+                                <h2>Our solution</h2>
+                                <p class="lead">Lorizzle ipsizzle dolizzle dang amet, yo adipiscing elit.</p>
+                                <p>Nullizzle sapizzle velizzle, aliquet volutpizzle, suscipit quis, gravida vizzle, nizzle. Pellentesque egizzle bling bling. Fo erizzle. I'm in the shizzle at dolor shiznit black tempus bling bling. Maurizzle pellentesque shizznit hizzle turpizzle. Izzle in i saw beyonces tizzles and my pizzle went crizzle. Black bizzle rhoncizzle break yo neck, yall. In fo shizzle my nizzle sheezy platea fo shizzle mah nizzle fo rizzle, mah home g-dizzle. Tellivizzle dapibizzle. Curabitur tellus urna, pretizzle ma nizzle, mattizzle funky fresh, eleifend vitae, nizzle. Sheezy suscipizzle. Check it out sempizzle mofo yo fo shizzle.</p>
 
+                            </div>
+                        </div>
+                    </div>
+                </section>
 
-  render() {
-    return (
-      <div>
-  
-Test
-        
-      </div>
-    )
-  }
+                <section id="services" class="bg-white">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-8 mx-auto">
+                                <h2>Our Team</h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut optio velit inventore, expedita quo laboriosam possimus ea consequatur vitae, doloribus consequuntur ex. Nemo assumenda laborum vel, labore ut velit dignissimos.</p>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        )
+    }
 }
