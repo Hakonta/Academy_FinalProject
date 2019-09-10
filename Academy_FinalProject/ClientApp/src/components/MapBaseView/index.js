035c24d0ddcfd9d2b5b8f798120bdaae649347f5
Cipo87 <jacob_aspelin@hotmail.com>
2019-09-10 09:47:10 +0200

    current marker

--- Academy_FinalProject/ClientApp/src/components/MapBaseView/index.js
+++ Academy_FinalProject/ClientApp/src/components/MapBaseView/index.js
@@ -182,7 +182,7 @@ export default class MapBaseLayer extends Component {
               }
             </MarkerClusterer> 
             
-              <CurrentPositionMarker currentPosition={this.state.currentCenter}/> 
+              <CurrentPositionMarker position={this.state.currentCenter}/> 
 
              {/* {this.state.selectedScooter && (
               <InfoWindow
