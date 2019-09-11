5ef62fe521b957c0dd99e74df37fddbf80bdef2f
Cipo87 <jacob_aspelin@hotmail.com>
2019-09-11 13:14:00 +0200

    WOrked on Card navbar

--- Academy_FinalProject/ClientApp/src/Components/CardNavbar/index.js
+++ Academy_FinalProject/ClientApp/src/Components/CardNavbar/index.js
@@ -1,47 +1,56 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import '../../Styles/style.css'
+import ExitIcon from '../../Assets/exitIcon.png'
 
 const cardNavbar = () => {
 
-    const [isAccountClicked, setisAccountClicked] = useState(false);
-    const [isSafetyClicked, setisSafetyClicked] = useState(false);
-    const [isAboutClicked, setisAboutClicked] = useState(false);
-    const [isContactCliced, setisContactCliced] = useState(false);
+    // const [isAccountClicked, setisAccountClicked] = useState(false);
+    // const [isSafetyClicked, setisSafetyClicked] = useState(false);
+    // const [isAboutClicked, setisAboutClicked] = useState(false);
+    // const [isContactCliced, setisContactCliced] = useState(false);
     return (
         <div className={'cardNavbar'}>
             <Link
                 to="/account"
-                className={isAccountClicked ? 'cardNavbarLinkActive' : 'cardNavbarLink'}
-                onClick={() => { setisAccountClicked(!isAccountClicked) }}
+                className={'cardNavbarLink'}
+            // className={isAccountClicked ? 'cardNavbarLinkActive' : 'cardNavbarLink'}
+            //onClick={() => { setisAccountClicked(!isAccountClicked) }}
             >
                 My Account
             </Link>
             <Link
                 to="/safety"
-                className={isSafetyClicked ? 'cardNavbarLinkActive' : 'cardNavbarLink'}
-                onClick={() => { setisSafetyClicked(!isSafetyClicked) }}
+                className={'cardNavbarLink'}
+
+            //className={isSafetyClicked ? 'cardNavbarLinkActive' : 'cardNavbarLink'}
+            //onClick={() => { setisSafetyClicked(!isSafetyClicked) }}
             >
                 Safety
             </Link>
             <Link
                 to="/about"
-                className={isAboutClicked ? 'cardNavbarLinkActive' : 'cardNavbarLink'}
-                onClick={() => { setisAboutClicked(!isAboutClicked) }}
+                className={'cardNavbarLink'}
+
+            //className={isAboutClicked ? 'cardNavbarLinkActive' : 'cardNavbarLink'}
+            //onClick={() => { setisAboutClicked(!isAboutClicked) }}
             >
                 About SQT
              </Link>
             <Link
                 to="/contact"
-                className={isContactCliced ? 'cardNavbarLinkActive' : 'cardNavbarLink'}
-                onClick={() => { setisContactCliced(!isContactCliced) }}
+                className={'cardNavbarLink'}
+
+            // className={isContactCliced ? 'cardNavbarLinkActive' : 'cardNavbarLink'}
+            //onClick={() => { setisContactCliced(!isContactCliced) }}
             >
                 Contact us
             </Link>
             <Link
                 to="/"
+                
             >
-                APP
+                <img src={ExitIcon} className={'cardNavbarExitIcon'} />
         </Link>
         </div>
     )
