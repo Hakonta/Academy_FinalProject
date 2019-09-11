38edcae3fa1e6836b8cbb66e1738f4a425144f2a
Cipo87 <jacob_aspelin@hotmail.com>
2019-09-11 11:42:56 +0200

    navbar work

---
+++ Academy_FinalProject/ClientApp/src/Containers/ContactView/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import CardNavBar from '../../Components/CardNavbar'
+
+const saftyView = () => {
+    <React.Fragment>
+        <CardNavBar/>
+    </React.Fragment>
+}
+export default accountView;
