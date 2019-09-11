c103bec32cb4acd66ac6294cf106b7102b21db84
Cipo87 <jacob_aspelin@hotmail.com>
2019-09-11 13:22:46 +0200

    merge

--- Academy_FinalProject/ClientApp/src/components/HeaderBar/index.js
+++ Academy_FinalProject/ClientApp/src/components/HeaderBar/index.js
@@ -54,22 +54,14 @@ export class HeaderBar extends React.Component {
           <a href="/" onClick={this.closeNavClick} className="close-nav">
             &times;
           </a>
-<<<<<<< HEAD
-          <a>{<img src={logo} alt={''} height='60x' width='100px' />}</a>
-=======
           <a>{<img src={logo} alt={''} height='60x' width='100px'/>}</a>
-          <br></br>
-          <a href="/account">My Account</a>
-          <a href="/safety">Safety</a>
-          <a href="/about">About SQT</a>
-          <a href="/contact">Report a problem</a>
           
->>>>>>> Added font to menu
+          
 
           <Link to="/account">My Account</Link>
           <Link to="/safety">Safety</Link>
           <Link to="/about">About SQT</Link>
-          <Link to="/contact">Contact us</Link>
+          <Link to="/contact">Report a problem</Link>
         </div>
 
           <button
