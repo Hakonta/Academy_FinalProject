8d1a2f3062fba3e3bdea5ebf8462f25de8a3fb64
Cipo87 <jacob_aspelin@hotmail.com>
2019-09-11 13:19:03 +0200

    cardnavbar

--- Academy_FinalProject/ClientApp/src/Components/CardNavbar/index.js
+++ Academy_FinalProject/ClientApp/src/Components/CardNavbar/index.js
@@ -5,53 +5,36 @@ import ExitIcon from '../../Assets/exitIcon.png'
 
 const cardNavbar = () => {
 
-    // const [isAccountClicked, setisAccountClicked] = useState(false);
-    // const [isSafetyClicked, setisSafetyClicked] = useState(false);
-    // const [isAboutClicked, setisAboutClicked] = useState(false);
-    // const [isContactCliced, setisContactCliced] = useState(false);
     return (
         <div className={'cardNavbar'}>
             <Link
                 to="/account"
                 className={'cardNavbarLink'}
-            // className={isAccountClicked ? 'cardNavbarLinkActive' : 'cardNavbarLink'}
-            //onClick={() => { setisAccountClicked(!isAccountClicked) }}
             >
                 My Account
             </Link>
             <Link
                 to="/safety"
-                className={'cardNavbarLink'}
-
-            //className={isSafetyClicked ? 'cardNavbarLinkActive' : 'cardNavbarLink'}
-            //onClick={() => { setisSafetyClicked(!isSafetyClicked) }}
-            >
+                className={'cardNavbarLink'}>
                 Safety
             </Link>
             <Link
                 to="/about"
                 className={'cardNavbarLink'}
-
-            //className={isAboutClicked ? 'cardNavbarLinkActive' : 'cardNavbarLink'}
-            //onClick={() => { setisAboutClicked(!isAboutClicked) }}
             >
                 About SQT
              </Link>
             <Link
                 to="/contact"
                 className={'cardNavbarLink'}
-
-            // className={isContactCliced ? 'cardNavbarLinkActive' : 'cardNavbarLink'}
-            //onClick={() => { setisContactCliced(!isContactCliced) }}
             >
                 Contact us
             </Link>
             <Link
                 to="/"
-                
             >
                 <img src={ExitIcon} className={'cardNavbarExitIcon'} />
-        </Link>
+            </Link>
         </div>
     )
 }
