f7107909b850b33244d63a08cb868e0429fa9481
rebekkavaa <rebekka.v.aa@gmail.com>
2019-09-12 16:33:29 +0200

    edited fonts

--- Academy_FinalProject/ClientApp/src/Containers/AccountView/index.js
+++ Academy_FinalProject/ClientApp/src/Containers/AccountView/index.js
@@ -59,9 +59,9 @@ export default class safetyView extends Component {
                         <div class={'rowTop'}>
                             <div className={'wrappersafety'}>
                                 <br></br>
-                                <h2 className={'safetyHeader'}>Welcome Ania</h2>
+                                <h2 className={'safetyHeader' }style = {{fontFamily:"'Red Hat Display', sans-serif"}}>Welcome Ania</h2>
                                 <p class="lead"><b>My recent rides:</b></p><ul>
-                                    <li><b>13/11/2019 15:11 - 15:12 <br></br></b>Rode a Tier - <a href="">More info</a></li><li><b>12/11/2019 17:42 - 17:56 <br></br></b>Rode a Zvipp - <a href="">More info</a></li><li><b>09/11/2019 10:40 - 10:58 <br></br></b>Rode a Voi - <a href="">More info</a></li>
+                                    <li style = {{fontFamily:"'Red Hat Display', sans-serif"}}><b>13/11/2019 15:11 - 15:12 <br></br></b>Rode a Tier - <a href="">More info</a></li><li><b>12/11/2019 17:42 - 17:56 <br></br></b>Rode a Zvipp - <a href="">More info</a></li><li><b>09/11/2019 10:40 - 10:58 <br></br></b>Rode a Voi - <a href="">More info</a></li>
                                 </ul>
                                 <p class="lead"><b>You have earned <b>560</b> points. Earn more points for a free ride!</b></p>
                                 <p>As a registered member, you'll earn points after each paid ride. Use your points to unlock free minutes of ride time. <br></br>
