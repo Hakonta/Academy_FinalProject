b2f0a12129feef7f12f015444549e7d25effa377
rebekkavaa <rebekka.v.aa@gmail.com>
2019-09-12 17:42:10 +0200

    edited about

--- Academy_FinalProject/ClientApp/src/Containers/AboutView/index.js
+++ Academy_FinalProject/ClientApp/src/Containers/AboutView/index.js
@@ -38,22 +38,29 @@ export default class AboutView extends Component {
                 <div class={'outerContainerAbout'}>
 
 
-                <section id={'about'}>
-                    <div class={'aboutViewContainerTop'}>
-                        <div class={'rowTop'}>
-                            <div className={'wrapperAbout'}>
-                                <br></br>
-                                <h2 className={'aboutHeader'} style = {{fontFamily:"'Red Hat Display', sans-serif"}}>About</h2>
-                                <p class="lead" style = {{fontFamily:"'Red Hat Display', sans-serif"}}><b>So what's this about?</b></p>
-                                <p>SQT is the lovechild of a couple of students who attended <a className={'aboutLinks'} href="https://www.academy.no/">Academy</a>(summer of 2019). It was developed in a two week project for our finals.
-                                Prior to the three months we've spent learning coding during our time at Academy, most of us had none or almost no experience with coding.
+                    <Plx
+                        className='MyAwesomeParallax'
+                        parallaxData={parallaxData} // your parallax effects, see documentation
+                    >
+                        <img class={'testImageAbout'} src={about} width="100%" alt="" />
+                    </Plx>
+
+                    <section id={'about'}>
+                        <div class={'aboutViewContainerTop'}>
+                            <div class={'rowTop'}>
+                                <div className={'wrapperAbout'}>
+                                    <br></br>
+                                    <h2 className={'aboutHeader'} style={{ fontFamily: "'Red Hat Display', sans-serif" }}>About</h2>
+                                    <p class="lead"><b>So what's this about?</b></p>
+                                    <p>SQT is the lovechild of a couple of students who attended <a className={'aboutLinks'} href="https://www.academy.no/">Academy</a>(summer of 2019). It was developed in a two week project for our finals.
+                                    Prior to the three months we've spent learning coding during our time at Academy, most of us had none or almost no experience with coding.
                                 It's been a fun ride and we hope you like our little project! If you're curious about the techincal aspects of things, you can check out our Github page <a className={'aboutLinks'} href="https://github.com/Hakonta/Academy_FinalProject">here</a>.
                                 <br></br>
                                     </p>
                                     <p class="lead"><b>Why SQT?</b></p>
                                     <p>If you've ever tried to book an e-scooter before, you've most likely ran into to this problem: So many providers(and apps!),
                                         but you just want the scooter that's the closest to where you are at the moment.
-                                    It'd be so much easier if you could have them all on the same map. Well, now you can!</p>
+                                    It'd be so much easier if you could have them all on the same map. Well, now you can! </p>
 
                                 </div>
                             </div>
